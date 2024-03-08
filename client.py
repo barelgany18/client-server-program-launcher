9a3e201a9a2e2fe183e42e09c9003d9b2c1c9dc3
barelgany18 <barelgany1818@gmail.com>
2024-03-08 16:14:15 +0200

    coordinates click updtae
    added the option to send coordinates from the client side to be clicked on server the side

--- client.py
+++ client.py
@@ -3,11 +3,25 @@ import socket
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 my_socket.connect(("127.0.0.1", 8820))
 
-while True:
-    msg = input("Please enter your message\n")
-    my_socket.send(msg.encode())
-    if msg == "exit" or msg is None or msg == "":
-        break
+print("p \t Run program")
+print("c \t Press coordinates")
+print("q \t Quit")
+choice = input("Please enter your choice: ")
+while choice != 'q':
+    if choice == 'p':
+        program_name = input("Enter program name: ")
+        msg = "p" + "-" + program_name
+        my_socket.send(msg.encode())
+    elif choice == 'c':
+        x_coordinates = input("Enter X coordinates: ")
+        y_coordinates = input("Enter Y coordinates: ")
+        msg = "c" + "-" + x_coordinates + "-" + y_coordinates
+        my_socket.send(msg.encode())
+
+    print("p \t Run program")
+    print("c \t Press coordinates")
+    print("q \t Quit")
+    choice = input("Please enter your choice: ")
 
 print("Goodbye")
 my_socket.close()
--- server.py
+++ server.py
@@ -1,23 +1,30 @@
 import socket
 import os
+import pyautogui
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind(("0.0.0.0", 8820))
 server_socket.listen()
 print("Server is up and running")
 
-
 while True:
     (client_socket, client_address) = server_socket.accept()
     print("Client connected", client_address)
     while True:
         data = client_socket.recv(1024).decode()
-        if data == "exit" or data is None or data == "":
+        if data[0] == "q" or data[0] is None:
             client_socket.close()
             print("Client disconnected")
             break
-        try:
-            os.system(data + '.exe')
-            client_socket.send("".encode())
-        except Exception as e:
-            print(e)
+        split_data = data.split("-")
+        if data[0] == 'p':
+            try:
+                os.system(split_data[1] + '.exe')
+            except Exception as e:
+                print(e)
+
+        elif data[0] == 'c':
+            if pyautogui.onScreen(int(split_data[1]), int(split_data[2])):
+                pyautogui.click(int(split_data[1]), int(split_data[2]))
+            else:
+                print("coordinates are not within the screen’s boundaries")
